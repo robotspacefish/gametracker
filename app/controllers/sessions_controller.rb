3f2760a60f0ad490f4482138ce9fa3aeabec19b0
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-01-13 13:10:35 -0500

    replace urls with current user page helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,13 @@ class SessionsController < ApplicationController
     if !logged_in?
       erb :'/sessions/login'
     else
-      redirect "#{current_user_page}"
+      redirect current_user_page
     end
   end
 
   post '/login' do
     login(params[:user])
-    redirect "#{current_user_page}"
+    redirect current_user_page
   end
 
   get '/logout' do
@@ -21,7 +21,7 @@ class SessionsController < ApplicationController
     if !logged_in?
       erb :'/sessions/signup'
     else
-      redirect "#{current_user_page}"
+      redirect current_user_page
     end
   end
 end
