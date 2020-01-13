096d13eee3f2fcabd3baa6987cd83229bea8a923
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-01-12 22:58:11 -0500

    set up helpers for login, logout, and current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,18 @@ class ApplicationController < Sinatra::Base
   get "/" do
     erb :index
   end
+  helpers do
+    def logged_in?
+      !!current_user
+    end
 
+    def current_user
+      @current_user = "test"
+      # @current_user ||= User.find_by(username: session[:username] if session[:username])
+    end
+
+    def logout!
+      session.clear
+    end
+  end
 end
