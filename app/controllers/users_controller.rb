a9b2c5b3a4faa16ff1eb230945ab38a9342f6837
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-01-13 12:23:41 -0500

    logic to show user or error page based on log in status

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   get '/users/:slug' do
     @user = User.find_by_slug(params[:slug])
-    erb :'/users/show'
+
+    if logged_in? && current_user.slug == params[:slug]
+      erb :'/users/show'
+    else
+      erb :'/users/error'
+    end
   end
 end
