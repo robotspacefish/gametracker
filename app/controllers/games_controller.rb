7d68bef8b926cd245d42fb3b3e4d7660ef055303
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-01-14 12:27:57 -0500

    add custom game to all games & user library or return error message

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -26,7 +26,19 @@ class GamesController < ApplicationController
         @user_has_game = !!current_user.games.find_by(id: @existing_game.id)
         erb :'games/add_existing_game'
       else
-
+          #TODO verify content before adding
+          # if it doesn't exist, create it & add to all games
+          # and add to current user's library
+          @game = current_user.games.build(params[:game])
+          if current_user.save
+            redirect to current_user_page
+          else
+            # redirect to '/games/new'
+            <<-ERROR
+              There was a problem adding this game.
+              <a href=/games/new>Back to Add Custom Game</a>
+            ERROR
+          end
       end
     end
   end
