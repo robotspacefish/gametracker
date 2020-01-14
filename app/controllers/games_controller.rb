77a4c22e74cd60384c3f8690411b26f4511f94ab
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-01-14 09:52:35 -0500

    post route todos

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,6 +8,15 @@ class GamesController < ApplicationController
     erb :'/games/new'
   end
 
+  post '/games/' do
+    # TODOs
+    # check for current user logged in
+    # check db for game
+    # if it doesn't exist, create it & add to all games
+    # add to current user's library
+    # reroute to
+  end
+
   get '/games/:slug' do
     @game = Game.find_by_slug(params[:slug])
 
