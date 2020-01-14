dfe7552377a43e00b2e63f16ba2cecaf2792c2b4
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-01-14 13:13:21 -0500

    handle blank title redirect

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -20,27 +20,34 @@ class GamesController < ApplicationController
     if !logged_in?
       redirect '/login'
     else
-      # check db for game
-      @existing_game = Game.where("title LIKE ?", params[:game][:title])[0]
+      title = params[:game][:title]
 
-      if @existing_game
-        @user_has_game = !!current_user.games.find_by(id: @existing_game.id)
-        erb :'games/add_existing_game'
+      if title.blank?
+        # TODO error "Title cannot be blank"
+        redirect '/games/new'
       else
-          #TODO verify content before adding
-          # if it doesn't exist, create it & add to all games
-          # and add to current user's library
-          @game = current_user.games.build(params[:game])
-          if current_user.save
-            redirect to current_user_page
-          else
-            # redirect to '/games/new'
-            <<-ERROR
-              There was a problem adding this game.
-              <a href=/games/new>Back to Add Custom Game</a>
-            ERROR
-          end
+        # check db for game
+        @existing_game = Game.where("title LIKE ?", title)[0]
+
+        if @existing_game
+          @user_has_game = !!current_user.games.find_by(id: @existing_game.id)
+          erb :'games/add_existing_game'
+        else
+            # if it doesn't exist, create it & add to all games
+            # and add to current user's library
+            @game = current_user.games.build(params[:game])
+            if current_user.save
+              redirect to current_user_page
+            else
+              # redirect to '/games/new'
+              <<-ERROR
+                There was a problem adding this game.
+                <a href=/games/new>Back to Add Custom Game</a>
+              ERROR
+            end
+        end
       end
+
     end
   end
 
