70a5325be6cf77290e740a5fbfff45e63fbddea5
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-01-15 13:27:17 -0500

    temporary db seeds

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+# IgdbApi.platforms
+
+User.create(username: "jess", password: "pw")
+User.create(username: "robotspacefish", password: "pw")
+
+Game.create(title: "Mass Effect")
+Game.create(title: "Mass Effect 2")
+Game.create(title: "Mass Effect 3")
+Game.create(title: "The Legend of Zelda")
+Game.create(title: "Gears of War")
+Game.create(title: "Hollow Knight")
+
+Platform.create(name: "Playstation 4")
+Platform.create(name: "Xbox One")
+Platform.create(name: "Nintendo Switch")
+Platform.create(name: "PC")
