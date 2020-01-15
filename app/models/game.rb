706de4ea534f43190fe2dc74215c9eae04ab8326
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-01-15 14:43:34 -0500

    fix: wrote association wrong

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,7 +3,7 @@ class Game < ActiveRecord::Base
   has_many :users, through: :game_users
 
   has_many :game_platforms
-  has_many :games, through: :game_platforms
+  has_many :platforms, through: :game_platforms
 
   def slug
     self.title.downcase.gsub(" ", "-")
