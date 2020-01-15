13991f683becb072a19b37b0fec4ed0da0845975
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-01-15 11:23:30 -0500

    update game relationships with platforms

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,9 @@ class Game < ActiveRecord::Base
   has_many :user_games
   has_many :users, through: :user_games
 
+  has_many :game_platforms
+  has_many :games, through: :game_platforms
+
   def slug
     self.title.downcase.gsub(" ", "-")
   end
