1ff7585e4a63ec9b37fddc171ee8a0917889fdcd
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-01-15 20:39:37 -0500

    move methods to create db records to Platform model

--- app/igdb_api.rb
+++ app/igdb_api.rb
@@ -12,20 +12,8 @@ class IgdbApi
 
     request.body = "fields game.parent_game.name, game.name, game.platforms.name, game.collection.name, game.collection.slug, game.collection.games.name, game.cover.url, game.cover.height, game.cover.width, game.cover.image_id, game.release_dates.human, game.release_dates.platform.name, game.release_dates.region, game.franchise, game.total_rating, game.url, game.time_to_beat.normally, game.time_to_beat.completely, game.screenshots.url, game.screenshots.width, game.screenshots.height, game.screenshots.image_id, game.genres.name, game.genres.slug, game.summary, game.slug; search \"#{game_title}\"; limit 50;"
 
-    JSON.parse(http.request(request).body)
-  end
-
-  def self.platforms
-    platforms = self.retrieve_platforms_from_api
+    result = JSON.parse(http.request(request).body)
     binding.pry
-    self.add_platforms_to_db(platforms)
-  end
-
-  def self.add_platforms_to_db(platforms)
-    # TODO
-    platforms.each do |platform|
-      Platform.create(platform)
-    end
   end
 
   def self.retrieve_platforms_from_api
@@ -34,7 +22,7 @@ class IgdbApi
     path = 'https://api-v3.igdb.com/platforms'
     request = Net::HTTP::Post.new(URI(path), {'user-key' => $api_key})
 
-    request.body = 'fields abbreviation, name, product_family.name, product_family.slug, platform_logo, platform_logo.height, platform_logo.width, platform_logo.image_id, platform_logo.url, slug; limit 500;'
+    request.body = 'fields abbreviation, name, slug; limit 500;'
 
     JSON.parse(http.request(request).body)
   end
--- app/models/platform.rb
+++ app/models/platform.rb
@@ -1,4 +1,15 @@
 class Platform < ActiveRecord::Base
   has_many :game_platforms
   has_many :games, through: :game_platforms
+
+  def self.setup_platforms
+    platforms = IgdbApi.retrieve_platforms_from_api
+    self.add_platforms_to_db(platforms)
+  end
+
+   def self.add_platforms_to_db(platforms)
+    platforms.each do |platform|
+      self.create(platform)
+    end
+  end
 end
