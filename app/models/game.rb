bf12df4a440d21d029d6d12b163ff29a64d27265
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-01-16 13:52:39 -0500

    method to get total # of a game title owned across all platforms

--- app/models/game.rb
+++ app/models/game.rb
@@ -12,6 +12,26 @@ class Game < ActiveRecord::Base
   def self.find_by_slug(slug)
     Game.all.find { |g| g.slug == slug }
   end
+
+  def total_owned_by_all_users_across_platforms
+    # get game/platforms relationship
+    game_platforms = self.game_platforms
+
+    # get relationship with users
+    game_ugp = game_platforms.collect do |gp|
+      UsersGamePlatform.where("game_platform_id = ?", gp.id)
+    end
+
+    # filter out all the empty Relations
+    total_users = game_ugp.filter { |ugp| !ugp.empty? }
+
+    total_users.length
+  end
+
+  def is_owned_by_user?(user)
+    !!user.games.include?(self)
+  end
+
   def self.find_uniq_games_by_username(username)
     user = User.find_by(username: username)
     user.games.uniq
