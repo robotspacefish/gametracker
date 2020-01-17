76bd8b1881a4d710b5f76870458d164b70b003ee
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-01-17 17:13:11 -0500

    fix typo and add .first to get first result

--- app/models/user_game_platform.rb
+++ app/models/user_game_platform.rb
@@ -3,6 +3,6 @@ class UsersGamePlatform < ActiveRecord::Base
   belongs_to :game_platform
 
   def self.find_by_ids(user_id, game_platform_id)
-    self.where("user_id = ? AND game_platform_id = ?", user_id, game_plafrom_id)
+    self.where("user_id = ? AND game_platform_id = ?", user_id, game_platform_id).first
   end
 end
