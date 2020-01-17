226560aa5ed180605a3f11f5b7c3dbacbe36ba41
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-01-16 22:25:42 -0500

    search results get and post routes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,17 @@
 class SearchController < ApplicationController
   post '/search/results' do
-    results = IgdbApi.search(params[:game][:title])
-      games_objects = IgdbApi.create_objects_from_parsed_data(results)
+    if !logged_in?
+      redirect '/login'
+    else
+      results = IgdbApi.search(params[:game][:title])
+      @games_objects = IgdbApi.create_objects_from_parsed_data(results)
 
-    # Game.create(
+      erb :'/search/results'
+    end
 
-    # )
   end
 
   get '/search/results' do
-
+    erb :'/search/results'
   end
 end
