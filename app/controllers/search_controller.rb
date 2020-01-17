b24eda97c9ea7248d9e25879891670ea7a60b355
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-01-17 13:19:56 -0500

    create games from search result & find results based on title separately

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,11 +6,16 @@ class SearchController < ApplicationController
       parsed_results = IgdbApi.search(params[:game][:title])
       game_objects = IgdbApi.create_objects_from_parsed_data(parsed_results)
 
-      @search_results = game_objects.collect do |game|
-        # todo check custom games with no igdb_id first?
-        Game.find_or_add_game_to_db(game)
-      end.compact!
-binding.pry
+      game_objects.each do |game|
+        if !Game.exists_in_db?(game[:igdb_id])
+          if game[:platforms] && !game[:platforms].empty?
+            Game.add_game_to_db(game)
+          end
+        end
+      end
+
+      @search_results = Game.find_search_results(params[:game][:title])
+
       erb :'/search/results'
     end
   end
