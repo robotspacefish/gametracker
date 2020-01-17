3b06fcd5ea4d79a681856b6578b770bb3b443fb8
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-01-16 20:37:19 -0500

    create game objects from parsed api data

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,11 @@
 class SearchController < ApplicationController
   post '/search' do
-    @results = IgdbApi.search(params[:game][:title])
-    binding.pry
+    results = IgdbApi.search(params[:game][:title])
+    games = IgdbApi.create_object_from_parsed_data(results)
+
+    # Game.create(
+
+    # )
   end
 
   get '/search/results' do
