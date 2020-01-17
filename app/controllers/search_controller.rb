00b06cf66a17254a6ade0a4051c16e5436834482
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-01-16 22:28:12 -0500

    todo message to incorporate db games in search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,7 @@ class SearchController < ApplicationController
     else
       results = IgdbApi.search(params[:game][:title])
       @games_objects = IgdbApi.create_objects_from_parsed_data(results)
-
+      # TODO check db for existing games to add to results - check by igdb_id & custom=true
       erb :'/search/results'
     end
 
