463012a0952b08f41fea38c0d280d0efd8b428d9
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-01-18 19:13:34 -0500

    shorten msg

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -94,7 +94,7 @@ class UsersController < ApplicationController
     else
       current_user.delete_game_from_library(params[:game][:platform_id], params[:game][:game_id])
 
-      flash[:message] = "#{params[:game][:title]} for #{params[:game][:platform_name]} was removed from your library."
+      flash[:message] = "#{params[:game][:title]} for #{params[:game][:platform_name]} was removed."
       redirect current_user_page
     end
   end
