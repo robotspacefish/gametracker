82de1ac99301529141eba7844018911b32a1f60f
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-05-04 23:42:20 -0400

    update environment file

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,17 @@
-ENV['SINATRA_ENV'] ||= "development"
+# ENV['SINATRA_ENV'] ||= "development"
+ENV['RACK_ENV'] ||= "development"
 
 require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
+Bundler.require(:default, ENV['RACK_ENV'])
 
 require 'rack-flash'
 
 require_all 'app'
 
+configure :development do
+  set :database, 'sqlite3:db/development.sqlite'
+end
+
+configure :production do
+  set :database, ENV['DATABASE_URL']
+end
